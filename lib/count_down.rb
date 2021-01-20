be9a0999ba5808254957ec5a0daff5bdc8c688aa
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-20 22:03:24 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,10 @@
 count_down = 0
 count = 10
 
-while count >= 1
+while count >= 1 do
   puts "#{count}"
     count = count - 1
-end
+    while count = 0 do
+      puts "Happy New Year!"
+    end
+    
